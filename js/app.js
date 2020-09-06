89f5ab65f298463c7aebb297834a9ee8d085be0d
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2020-09-05 23:09:07 -0500

    con verificación de repetidos

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ $(document).ready(function()
 var i= [6];
 var j= [6];
 var matriz= new Array(6);
-
+var matriz2= new Array(4);
+var matriz3= new Array(4);
+//console.log(matriz);
 /*-----------------------------
 // cambio entre dos colores indefinidamente
 ------------------------------*/
@@ -28,6 +30,8 @@ $(".btn-reinicio").on("click", function(){
 
 borrar_contenido();	
 llenar_juego();
+
+
 
 //borrar contenido anterior
 function borrar_contenido(){
@@ -55,8 +59,47 @@ i=[];
 //alert(matriz[2][2]); //--ok
 
 }//cierre función
+/*-------------------------
+//verificar coincidencias 
+-------------------------*/
 
-})
+//console.log(matriz);
+//filas
+for(m=0; m<=6;m++){
+for(n=0;n<=6;n++){
+if(matriz[m][n]==matriz[m][(n+1)]){
+	if(matriz[m][n]==matriz[m][(n+2)]){
+//console.log(matriz[m][n]);
+//console.log(m);
+//console.log(n);
+//console.log("f");
+matriz2[m]=[matriz[m][n],m,n,"f"];
+console.log(matriz2);
+}//cierre if
+}//cierre if
+}//cierre for n
+}//cierre for m
+//filas
+
+//columnas
+
+for(n=0;n<=6;n++){
+for(m=0;m<=4;m++){
+if(matriz[m][n]==matriz[(m+1)][n]){
+if(matriz[m][n]==matriz[(m+2)][n]){
+//console.log(matriz[m][n]);
+//console.log(m);
+//console.log(n);
+//console.log("c");
+matriz3[n]=[matriz[m][n],m,n,"c"];
+console.log(matriz3);
+	}//cierre if
+	
+
+	}//cierre if
+}// cierre for m
+}// cierre for n
+//columnas
 
 /*----------------------------
 // Función número aleatorio
@@ -68,4 +111,6 @@ i[n] = (Math.floor(i[n]));
 }//cierre for n
 }//cierre función número aleatorio
 
-})
+})//cierre inicio juego
+
+})//cierre general
